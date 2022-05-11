9cd50c5f5c26a362ec1986a6ec31285929770a2c
lamhan.vu <vulh91@gmail.com>
2022-05-11 17:28:18 +0700

    fix updateInventory

--- index.js
+++ index.js
@@ -20,15 +20,18 @@ const getSupplySources = (spApi) => withLog(() => spApi.callAPI({
   version: "v0"
 }));
 
-const getInventory = (spApi, { location, sku}) => withLog(() => spApi.callAPI({
+const getInventory = (spApi, { location, sku }) => withLog(() => spApi.callAPI({
   api_path: `/externalFulfillment/inventory/2021-01-06/locations/${location.supplySourceId}/skus/${sku}`,
   method: 'GET',
   version: 'v0'
 }))
 
 const updateInventory = (spApi, { location, sku, quantity }) => withLog(() => spApi.callAPI({
-  api_path: `/externalFulfillment/inventory/2021-01-06/locations/${location.supplySourceId}/skus/${sku}?quantity=${quantity}`,
   method: 'PUT',
+  api_path: `/externalFulfillment/inventory/2021-01-06/locations/${location.supplySourceId}/skus/${sku}`,
+  query: {
+    quantity
+  },
   headers: {
     'If-Match': '1',
     'If-Unmodified-Since': '1'
@@ -71,8 +74,8 @@ const spApi = new SellingPartnerAPI({
   const quantity = 15;
 
   for (const sku of skus) {
-    const result = await getInventory(spApi, { location, sku });
-    // updateInventory(spApi, { location, sku, quantity })
+    // const result = await getInventory(spApi, { location, sku });
+    const updateResult = await updateInventory(spApi, { location, sku, quantity })
   }
 })();
 
